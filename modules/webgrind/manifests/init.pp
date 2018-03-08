60231a48c28912b49b21e78b2a9643e7cb3e301c
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-08 12:35:52 +1000

    Add support for disabled_extensions

--- modules/webgrind/manifests/init.pp
+++ modules/webgrind/manifests/init.pp
@@ -3,6 +3,12 @@ class webgrind (
 	$config,
 	$path   = '/vagrant/extensions/webgrind',
 ) {
+	if ( ! empty( $config[disabled_extensions] ) and 'chassis/webgrind' in $config[disabled_extensions] ) {
+		$file = absent
+	} else {
+		$file = link
+	}
+
 	exec { 'clone webgrind':
 		path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 		command => 'git clone https://github.com/jokkedk/webgrind.git /vagrant/extensions/webgrind/source',
@@ -10,7 +16,7 @@ class webgrind (
 		unless  => 'test -d /vagrant/extensions/webgrind/source'
 	}
 	file { '/vagrant/webgrind':
-		ensure => link,
+		ensure => $file,
 		target => '/vagrant/extensions/webgrind/source',
 		notify => Service['nginx'],
 	}
